39af934d85a46d47b328073432c27ba9aa6eaff8
Colin Sullivan <colinsullivan03@gmail.com>
2022-08-15 15:59:29 -0500

    Updated teh base classes.

--- src/dataclasses/base.py
+++ src/dataclasses/base.py
@@ -1,5 +1,6 @@
 
 from abc import ABC, abstractstaticmethod
+from typing import Optional, TypedDict
 
 from .. import Client
 
@@ -7,11 +8,12 @@ class BaseDataClass(ABC):
     """
     The base class used for all dataclasses.
     """
-    def __init__(self, client: Client, id: int):
+    def __init__(self, client: Client, id: int, data: Optional[TypedDict] = None):
         self.client = client
         self.rec_net = client.rec_net
         self.id = id
+        if data is not None: self.patch_data(data)
 
     @abstractstaticmethod
-    def patch_data(self, data: dict) -> None:
+    def patch_data(self, data: TypedDict) -> None:
         pass
--- src/managers/base_manager.py
+++ src/managers/base_manager.py
@@ -1,12 +1,28 @@
 from abc import ABC
-from typing import TypedDict
+from typing import Optional, TypedDict, Type
 
 from .. import Client
+from ..dataclasses import BaseDataClass
 
 class BaseManager(ABC):
     """
     The base class used by all managers.
     """
+
+    dataclass: Type[BaseDataClass]
+
     def __init__(self, client: Client):
         self.client = client
         self.rec_net = client.rec_net
+
+    def create_dataclass(self, id: int, data: Optional[TypedDict] = None) -> BaseDataClass:
+        """
+        Creates an object from the managers corresponding dataclass.
+
+        @param id: The unique number associated with each data response.
+        @param data: The data from an API response associated with the dataclass.
+        @return: Returns an object representing the data from the data response.
+        """
+        return self.dataclass(id, data)
+
+    
