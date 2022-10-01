4154814c1ac86cb14ee601f088b24d816c96cf56
Colin Sullivan <colinsullivan03@gmail.com>
2022-10-01 18:03:47 -0500

    Fixed Example 1

--- examples/example01.py
+++ examples/example01.py
@@ -1,7 +1,13 @@
-from ..src import Client
+from asyncio import get_event_loop
+
+from src import Client
 
 async def main():
     RecNet = Client()
     user = await RecNet.accounts.get("ColinXYZ")
     bio = await user.get_bio()
     print(bio)
+    await RecNet.close()
+
+loop = get_event_loop()
+loop.run_until_complete(main())
