588ab39b8163c56c6af02d2d3343286dcd8fad10
Jegarde <jesse.tapani.nieminen@gmail.com>
2022-10-02 23:11:03 +0300

    Make account manager handle empty data

--- src/managers/account_manager.py
+++ src/managers/account_manager.py
@@ -8,30 +8,38 @@ if TYPE_CHECKING:
     from ..rest import Response
 
 class AccountManager(BaseManager['Account', 'AccountResponse']):
-    async def get(self, name: str) -> 'Account':
+    async def get(self, name: str) -> Optional['Account']:
         """
         Gets user data by their username, and returns it as an account object.
+        Returns nothing if the account doesn't exist.
 
         @param name: The username of the RecNet user.
-        @return: An account object representing the data. 
+        @return: An account object representing the data or nothing if not found. 
         """
+        if not isinstance(name, str): return None
         data: 'Response[AccountResponse]' = await self.rec_net.accounts.account.make_request('get', params = {'username': name})
-        return self.create_dataclass(data.data['accountId'], data.data)
+        if data.data: return self.create_dataclass(data.data['accountId'], data.data)
+        return None
 
-    async def fetch(self, id: int) -> 'Account':
+    async def fetch(self, id: int) -> Optional['Account']:
         """
         Gets user data by their id, and returns it as an account object.
+        Returns nothing if an account with the specified id doesn't exist.
 
         @param id: The id of the RecNet user.
-        @return: An account object representing the data. 
+        @return: An account object representing the data or nothing if not found. 
         """
+        if not isinstance(id, int) or id < 1: return None
         data: 'Response[AccountResponse]' = await self.rec_net.accounts.account(id).make_request('get')
-        return self.create_dataclass(id, data.data)
+        if data.data: return self.create_dataclass(id, data.data)
+        return None
+        
 
     async def get_many(self, names: List[str]) -> List['Account']:
         """
         Gets a list of users by a list of usernames, and returns 
         a list of account object.
+        Accounts that couldn't be found will be silently ignored.
 
         @param names: A list of username.
         @return: A list of account objects. 
@@ -43,17 +51,20 @@ class AccountManager(BaseManager['Account', 'AccountResponse']):
         """
         Gets a list of users by a list of ids, and returns 
         a list of account object.
+        Accounts that couldn't be found will be silently ignored.
 
         @param ids: A list of ids.
         @return: A list of account objects. 
         """
-        data: 'Response[List[AccountResponse]]' = await self.rec_net.accounts.account.bulk.make_request('post', body = {'id': ids})
+        purified_ids = list(filter(lambda id: isinstance(id, int), ids))
+        data: 'Response[List[AccountResponse]]' = await self.rec_net.accounts.account.bulk.make_request('post', body = {'id': purified_ids})
         return self.create_from_data_list(data.data)
 
     async def search(self, query: str) -> List['Account']:
         """
         Searches RecNet for users based on a query, and returns
         a list of account objects.
+        If no account is found, an empty list will be returned.
 
         @param query: A search query string.
         @return: A list of account objects.
