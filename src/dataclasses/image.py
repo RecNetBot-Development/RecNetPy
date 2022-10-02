2f9f5f11f1381053c7c572dc98f38e7ea6a8af36
Colin Sullivan <colinsullivan03@gmail.com>
2022-10-02 10:36:40 -0500

    Added The Rest Of The Dataclass Functions.

--- src/dataclasses/image.py
+++ src/dataclasses/image.py
@@ -1,12 +1,13 @@
-from typing import TYPE_CHECKING, List, Optional, Union
+from typing import TYPE_CHECKING, List, Optional, Dict
 
 from .base import BaseDataClass
+from .comment import Comment
 from ..misc import date_to_unix
 from ..misc.constants import ACCESSIBILITY_DICT
 
 if TYPE_CHECKING:
     from . import Account, Room, Event
-    from ..misc.api_responses import ImageResponse
+    from ..misc.api_responses import ImageResponse, CommentResponse, AccountResponse
     from ..rest import Response
 
 class Image(BaseDataClass['ImageResponse']):
@@ -25,13 +26,14 @@ class Image(BaseDataClass['ImageResponse']):
     player_event_id: Optional[int]
     created_at: int
     cheer_count: int
-    cheer_player_ids: Optional[List[int]] = None
-    cheer_players: Optional[List['Account']] = None
     comment_count: int
-    player: Optional['Account']
-    tagged_players: Optional[List['Account']]
-    room: Optional['Room']
-    player_event: Optional['Event']
+    player: Optional['Account'] = None
+    tagged_players: Optional[List['Account']] = None
+    room: Optional['Room'] = None
+    player_event: Optional['Event'] = None
+    cheer_player_ids: Optional[List[int]] = None
+    comments: Optional[List['Comment']] = None
+    cheer_players: Optional[List['Account']] = None
 
     def patch_data(self, data: 'ImageResponse') -> None:
         """
@@ -39,6 +41,7 @@ class Image(BaseDataClass['ImageResponse']):
 
         @param data: Data from the api.
         """
+        self.data = data
         self.id = data['Id']
         self.type = data['Type']
         self.accessibility = ACCESSIBILITY_DICT.get(data['Accessibility'], "Unknown")
@@ -52,6 +55,55 @@ class Image(BaseDataClass['ImageResponse']):
         self.created_at = date_to_unix(data['CreatedAt'])
         self.cheer_count = data['CheerCount']
         self.comment_count = data['CommentCount']
+
+    async def get_player(self, force: bool = False) -> 'Account':
+        """
+        Fetches the creator of the image. Returns a
+        cached result, if this function has been already called.
+
+        @param force: If true, fetches new data.
+        @return: An account object.
+        """
+        if self.player is None or force:
+            self.player = await self.client.accounts.fetch(self.player_id)
+        return self.player
+
+    async def get_tagged_players(self, force: bool = False) -> List['Account']:
+        """
+        Fetches account data for the player who appeared in the Image. Returns a
+        cached result, if this function has been already called.
+
+        @param force: If true, fetches new data.
+        @return: A list of account objects.
+        """
+        if self.tagged_players is None or force:
+            self.tagged_players = await self.client.accounts.fetch_many(self.tagged_player_ids)
+        return self.tagged_players
+
+    async def get_room(self, force: bool = False) -> 'Room':
+        """
+        Fetches the room the image was taked in. Returns a
+        cached result, if this function has been already called.
+
+        @param force: If true, fetches new data.
+        @return: A room object.
+        """
+        if self.room is None or force:
+            self.room = await self.client.rooms.fetch(self.room_id)
+        return self.room
+
+    async def get_player_event(self, force: bool = False) -> Optional['Event']:
+        """
+        Fetches the event the image was taken in. Returns a
+        cached result, if this function has been already called.
+
+        @param force: If true, fetches new data.
+        @return: An event object, or None if the image wasn't taken in an event.
+        """
+        if self.player_event_id is None: return None
+        if self.player_event is None or force:
+            self.player_event = await self.client.events.fetch(self.player_event_id)
+        return self.player_event
         
     async def get_cheers(self, force: bool = False) -> List[int]:
         """
@@ -60,12 +112,53 @@ class Image(BaseDataClass['ImageResponse']):
 
         @param force: If true, fetches new data.
         @return: A list of account ids.
-        """
-        
+        """        
         if self.cheer_player_ids is None or force:
             data: 'Response[List[int]]' = await self.rec_net.api.images.v1(self.id).cheers.make_request('get')
-            self.cheer_player_ids = data.data
-            
+            self.cheer_player_ids = data.data           
         return self.cheer_player_ids
 
-    
+    async def get_comments(self, force: bool = False) -> List['Comment']:
+        """
+        Fetches a list of comments made on the image. Returns a
+        cached result, if this function has been already called.
+
+        @param force: If true, fetches new data.
+        @return: An list of comment objects.
+        """
+        if self.comments is None:
+            data: 'Response[List[CommentResponse]]' = await self.rec_net.api.images.v1(self.id).comments.make_request('get')
+            self.comments = Comment.create_from_list(data.data)
+        return self.comments
+
+    async def resolve_cheers(self, force = False) -> List['Account']:
+        """
+        Fetches a list of player objects who cheered the post. Returns a
+        cached result, if this function has been already called.
+
+        @param force: If true, fetches new data.
+        @return: A list of account objects.
+        """  
+        if self.cheer_players is None or force:
+            player_ids = await self.get_cheers(force)
+            self.cheer_players = self.client.accounts.fetch_many(player_ids)
+        return self.cheer_players
+
+    async def resolve_commenters(self, force = False) -> List['Comment']:
+        """
+        Resolves the players who commented on an image. This function
+        will make an api call every time its used. It should only be used when 
+        updating the comment player attribute.
+
+        @param force: Forces new responses to be fetched.
+        @return: A list of comment objects. 
+        """
+        comments = await self.get_comments(force)
+        players: Dict[int, Account] = {}
+        for comment in comments:
+            player = self.client.accounts.create_dataclass(comment.player_id)
+            comment.player = player
+            players[comment.player_id] = player
+        data: 'Response[List[AccountResponse]]' = await self.rec_net.accounts.account.bulk.make_request('post', body = {id: players.keys})
+        for data_response in data.data: players.get(data_response['accountId']).patch_data(data_response)
+        return self.comments
--- src/dataclasses/room.py
+++ src/dataclasses/room.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, List, Optional, Dict
 
 from .base import BaseDataClass
 from .subroom import SubRoom
@@ -11,8 +11,9 @@ from ..misc import bitmask_decode, date_to_unix
 from ..misc.constants import ACCESSIBILITY_DICT
 
 if TYPE_CHECKING:
-    from . import Account
-    from ..misc.api_responses import RoomResponse
+    from . import Account, Image, Event
+    from ..misc.api_responses import RoomResponse, AccountResponse
+    from ..rest import Response
 
 
 WARNING_MASK_LIST: List[str] = ["Custom", "Spooky/scary themes", "Mature themes", "Bright/flashing lights", "Intense motion", "Gore/violence"]
@@ -53,14 +54,17 @@ class Room(BaseDataClass['RoomResponse']):
     favorite_count: int
     visitor_count: int
     visit_count: int
-    creator_account: Optional['Account']
-    subrooms: Optional[List['SubRoom']]
-    roles: Optional[List['Role']]
-    tags: Optional[List['Tag']]
-    promo_images: Optional[List[str]]
-    promo_external_content: Optional[List['PromoExternalContent']]
-    scores: Optional[List['Score']]
-    load_screens: Optional[List['LoadScreen']]
+    creator_account: Optional['Account'] = None
+    subrooms: Optional[List['SubRoom']] = None
+    roles: Optional[List['Role']] = None
+    tags: Optional[List['Tag']] = None
+    promo_images: Optional[List[str]] = None
+    promo_external_content: Optional[List['PromoExternalContent']] = None
+    scores: Optional[List['Score']] = None
+    load_screens: Optional[List['LoadScreen']] = None
+    images: Optional[List['Image']] = None
+    events: Optional[List['Event']] = None
+
 
     def patch_data(self, data: 'RoomResponse') -> None:
         """
@@ -106,4 +110,65 @@ class Room(BaseDataClass['RoomResponse']):
         self.promo_images = data.get("PromoImages")
         self.promo_external_content = PromoExternalContent.create_from_list(data.get("PromoExternalContent"))
         self.scores = Score.create_from_list(data.get("Scores"))
-        self.load_screens = LoadScreen.create_from_list(data.get("LoadScreens"))        
+        self.load_screens = LoadScreen.create_from_list(data.get("LoadScreens")) 
+
+    async def get_images(self, take: int = 16, skip: int = 0, sort: int = 0, force: bool = False) -> List['Image']:
+        """
+        Fetches a list of images taken in this room. Returns a
+        cached result, if this function has been already called.
+
+        @param take: The number of results to return.
+        @param skip: The number of results to skip.
+        @param sort: An integer that describes how the results are to be sorted 
+        @param force: If true, fetches new data.
+        @return: A list of images.
+        """
+        if self.images is None or force:
+            self.images = await self.client.images.in_room(self.id)
+        return self.images
+
+    async def get_events(self, take: int = 16, skip: int = 0, force: bool = False) -> List['Event']:
+        """
+        Fetches a list of events happening in this room. Returns a
+        cached result, if this function has been already called.
+
+        @param take: The number of results to return.
+        @param skip: The number of results to skip.
+        @param force: If true, fetches new data.
+        @return: A list of events.
+        """
+        if self.events is None or force:
+            self.events = await self.client.events.in_room(self.id)
+        return self.events
+
+    async def get_creator_account(self, force: bool = False) -> 'Account':
+        """
+        Fetches the creator of the room. Returns a
+        cached result, if this function has been already called.
+
+        @param force: If true, fetches new data.
+        @return: An account object.
+        """
+        if self.creator_account is None or force:
+            self.creator_account = self.client.accounts.fetch(self.creator_account_id)
+        return self.creator_account
+
+    async def resolve_role_owners(self) -> Optional[List['Role']]:
+        """
+        Resolves the role owner for this room. This function
+        will make an api call every time its used. It should only be used when 
+        updating the role account attribute.
+
+        @return: A list of role objects, or None if roles is None 
+        """
+        if self.roles is None: return None
+        roles = self.roles
+        accounts: Dict[int, Account] = {}
+        for role in roles:
+            account = self.client.accounts.create_dataclass(role.account_id)
+            role.account = account
+            accounts[role.account_id] = account
+        data: 'Response[List[AccountResponse]]' = await self.rec_net.accounts.account.bulk.make_request('post', body = {id: accounts.keys})
+        for data_response in data.data: accounts.get(data_response['accountId']).patch_data(data_response)
+        return self.roles
+
