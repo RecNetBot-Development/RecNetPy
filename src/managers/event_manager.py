66968ef3ac158a195415e33f476227cd716d68ad
Jegarde <jesse.tapani.nieminen@gmail.com>
2022-10-05 13:55:26 +0300

    Acknowledge empty returns in EventManager

--- src/managers/event_manager.py
+++ src/managers/event_manager.py
@@ -8,20 +8,24 @@ if TYPE_CHECKING:
     from ..rest import Response
 
 class EventManager(BaseManager['Event', 'EventResponse']):
-    async def fetch(self, id: int) -> 'Event':
+    async def fetch(self, id: int) -> Optional['Event']:
         """
         Gets event data by their id, and returns it as an event object.
+        Returns nothing if the event doesn't exist or is private.
 
         @param id: The id of the event.
-        @return: An event object representing the data. 
+        @return: An event object representing the data or nothing if not found. 
         """
-        data: 'Response[EventResponse]' = await self.rec_net.api.playerevents.v1(id).make_request('get')
-        return self.create_dataclass(id, data.data)
+        data: 'Response[EventResponse]' = await self.rec_net.api.playerevents.v1(str(id)).make_request('get')
+        if data.data: return self.create_dataclass(id, data.data)
+        return None
+        
 
     async def fetch_many(self, ids: List[int]) -> List['Event']:
         """
         Gets a list of events by a list of event ids, and returns 
         a list of event object.
+        Events that couldn't be found will be silently ignored.
 
         @param ids: A list of ids.
         @return: A list of event objects. 
@@ -33,6 +37,7 @@ class EventManager(BaseManager['Event', 'EventResponse']):
         """
         Searches RecNet for events based on a query, and returns
         a list of event objects.
+        If no event is found, an empty list will be returned.
 
         @param query: A search query string.
         @param take: The number of results to return.
@@ -41,7 +46,7 @@ class EventManager(BaseManager['Event', 'EventResponse']):
         @return: A list of event objects.
         """
         params = {
-            'query': query,
+            'query': str(query),
             'take': take,
             'skip': skip,
             'sort': sort
@@ -52,6 +57,7 @@ class EventManager(BaseManager['Event', 'EventResponse']):
     async def from_account(self, id: int, take: int = 16, skip: int = 0) -> List['Event']:
         """
         Gets a list of events created by a player.
+        If no event is found, an empty list will be returned.
 
         @param id: An account id.
         @param take: The number of results to return.
@@ -62,12 +68,13 @@ class EventManager(BaseManager['Event', 'EventResponse']):
             'take': take,
             'skip': skip,
         }
-        data: 'Response[List[EventResponse]]' = await self.rec_net.api.playerevents.v1.creator(id).make_request('get', params=params)
+        data: 'Response[List[EventResponse]]' = await self.rec_net.api.playerevents.v1.creator(str(id)).make_request('get', params=params)
         return self.create_from_data_list(data.data)
 
     async def in_room(self, id: int, take: int = 16, skip: int = 0) -> List['Event']:
         """
         Gets a list of events happening in a room.
+        If no event is found, an empty list will be returned.
 
         @param query: A room id.
         @param take: The number of results to return.
@@ -78,12 +85,13 @@ class EventManager(BaseManager['Event', 'EventResponse']):
             'take': take,
             'skip': skip,
         }
-        data: 'Response[List[EventResponse]]' = await self.rec_net.api.playerevents.v1.room(id).make_request('get', params=params)
+        data: 'Response[List[EventResponse]]' = await self.rec_net.api.playerevents.v1.room(str(id)).make_request('get', params=params)
         return self.create_from_data_list(data.data)
 
     async def get_events(self, take: int = 16, skip: int = 0, sort: int = 0) -> List['Event']:
         """
         Gets a list of events currently happening.
+        If no event is found, an empty list will be returned.
 
         @param take: The number of results to return.
         @param skip: The number of results to skip.
