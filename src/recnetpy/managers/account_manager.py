784e98d5d9c6f4bd8e831629179e7edba301e4e5
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-07-24 00:08:45 +0300

    Default to APIM, include accounts fallback.

--- src/recnetpy/managers/account_manager.py
+++ src/recnetpy/managers/account_manager.py
@@ -20,8 +20,16 @@ class AccountManager(BaseManager['Account', 'AccountResponse']):
         :param name: The username of the RecNet user.
         :return: An account object representing the data or nothing if not found. 
         """
+        data: 'Response[AccountResponse]' = await self.rec_net.apim.accounts.account.make_request('get', params = {'username': str(name)})
+        if data.success and data.data: return self.create_dataclass(data.data['accountId'], data.data)
+
+        # If APIM gets deprecated, fall back to accounts host
+        print("APIM deprecated! File an issue on my repository. Check your input beforehand.")
         data: 'Response[AccountResponse]' = await self.rec_net.accounts.account.make_request('get', params = {'username': str(name)})
-        if data.data: return self.create_dataclass(data.data['accountId'], data.data)
+        if data.success and data.data: return self.create_dataclass(data.data['accountId'], data.data)
+
+        # No luck
+        print("APIM and Accounts deprecated! File an issue on my repository. Check your input beforehand.")
         return None
 
     async def fetch(self, id: int) -> Optional['Account']:
@@ -32,8 +40,16 @@ class AccountManager(BaseManager['Account', 'AccountResponse']):
         :param id: The id of the RecNet user.
         :return: An account object representing the data or nothing if not found. 
         """
+        data: 'Response[AccountResponse]' = await self.rec_net.apim.accounts.account(id).make_request('get')
+        if data.success and data.data: return self.create_dataclass(id, data.data)
+
+        # If APIM gets deprecated, fall back to accounts host
+        print("APIM deprecated! File an issue on my repository. Check your input beforehand.")
         data: 'Response[AccountResponse]' = await self.rec_net.accounts.account(id).make_request('get')
-        if data.data: return self.create_dataclass(id, data.data)
+        if data.success and data.data: return self.create_dataclass(id, data.data)
+
+        # No luck
+        print("APIM and Accounts deprecated! File an issue on my repository. Check your input beforehand.")
         return None
     
     async def get_many(self, names: List[str]) -> List['Account']:
@@ -46,8 +62,17 @@ class AccountManager(BaseManager['Account', 'AccountResponse']):
         :return: A list of account objects. 
         """
         bulk = stringify_bulk(names)
+        data: 'Response[List[AccountResponse]]' = await self.rec_net.apim.accounts.account.bulk.make_request('post', body = {'name': bulk})
+        if data.success: return self.create_from_data_list(data.data)
+
+        # If APIM gets deprecated, fall back to accounts host
+        print("APIM deprecated! File an issue on my repository. Check your input beforehand.")
         data: 'Response[List[AccountResponse]]' = await self.rec_net.accounts.account.bulk.make_request('post', body = {'name': bulk})
-        return self.create_from_data_list(data.data)
+        if data.success: return self.create_from_data_list(data.data)
+
+        # No luck
+        print("APIM and Accounts deprecated! File an issue on my repository. Check your input beforehand.")
+        return []
 
     async def fetch_many(self, ids: List[int]) -> List['Account']:
         """
@@ -58,8 +83,17 @@ class AccountManager(BaseManager['Account', 'AccountResponse']):
         :param ids: A list of ids.
         :return: A list of account objects. 
         """
+        data: 'Response[List[AccountResponse]]' = await self.rec_net.apim.accounts.account.bulk.make_request('post', body = {'id': ids})
+        if data.success: return self.create_from_data_list(data.data)
+    
+        # If APIM gets deprecated, fall back to accounts host
+        print("APIM deprecated! File an issue on my repository. Check your input beforehand.")
         data: 'Response[List[AccountResponse]]' = await self.rec_net.accounts.account.bulk.make_request('post', body = {'id': ids})
-        return self.create_from_data_list(data.data)
+        if data.success: return self.create_from_data_list(data.data)
+
+        # No luck
+        print("APIM and Accounts deprecated! File an issue on my repository. Check your input beforehand.")
+        return []
 
     async def search(self, query: str) -> List['Account']:
         """
@@ -71,7 +105,16 @@ class AccountManager(BaseManager['Account', 'AccountResponse']):
         :return: A list of account objects.
         """
         data: 'Response[List[AccountResponse]]' = await self.rec_net.apim.accounts.account.search.make_request('get', params = {'name': str(query)})
-        return self.create_from_data_list(data.data)
+        if data.success: return self.create_from_data_list(data.data)
+    
+        # If APIM gets deprecated, fall back to accounts host
+        print("APIM deprecated! File an issue on my repository. Check your input beforehand.")
+        data: 'Response[List[AccountResponse]]' = await self.rec_net.accounts.account.search.make_request('get', params = {'name': str(query)})
+        if data.success: return self.create_from_data_list(data.data)
+
+        # No luck
+        print("APIM and Accounts deprecated! File an issue on my repository. Check your input beforehand.")
+        return []
 
     def create_dataclass(self, id: int, data: Optional['AccountResponse'] = None) -> 'Account':
         """
